d2beb573010d32be95da42d213e12c1a257b7a82
pcuestamorer <pablocuestam@outlook.com>
2023-01-22 16:35:22 +0100

    fix: exception inputs

--- BattleshipGame/BattleshipGame/BattleshipGame/Main.cpp
+++ BattleshipGame/BattleshipGame/BattleshipGame/Main.cpp
@@ -54,92 +54,115 @@ void battle(Game game)
 			menuOp = 1;
 		} else
 		{
-			displayTurnsMenu();
-			cin >> menuOp;
+			bool validOp = false;
+			while (!validOp)
+			{
+				try
+				{
+					displayTurnsMenu();
+					cin >> menuOp;
+					if (cin.fail())
+					{
+						throw std::invalid_argument("The input is not a number");
+					} else if (menuOp < 1 || menuOp > 2)
+					{
+						throw std::invalid_argument("Choose an option between 1 and 2");
+					} else
+					{
+						validOp = true;
+					}
+				} catch (const std::invalid_argument& e)
+				{
+					system("cls");
+					cerr << "\033[1;31m" << e.what() << "\033[0m" << endl;
+					cin.clear();
+					cin.ignore(numeric_limits<::streamsize>::max(), '\n');
+					continue;
+				}
+			}
 			system("cls");
 		}
+		// Next move (pass turn)
 		if (menuOp == 1)
 		{
 			if (player1Turn)
 			{
-				// Display boards
-				player1.printShipsBoard();
-				player2.printShootingBoard();
-				// Ask for shooting coordinates
-				cout << player1.getName() << ", it's your turn to shoot. Enter coordinates (example: B3): ";
-				char row;
-				char column;
-				cin >> row >> column;
-				int x = row - 'A';
-				int y = column - '0';
-				try
+				bool validInput = false;
+				while (!validInput)
 				{
-					if (!isalpha(row) || !isupper(row) || row < 'A' || row > 'J')
-						throw invalid_argument("Invalid row coordinate. Please enter a valid letter between A and J in uppercase.");
-					if (!isdigit(column) || y < 0 || y > 9)
-						throw invalid_argument("Invalid column coordinate. Please enter a valid number between 0 and 9.");
-				} catch (const invalid_argument& e)
-				{
-					system("cls");
-					cerr << "\033[1;31m" << e.what() << "\033[0m" << endl;
-					continue;
-				}
-				// If player1 shoot player2. Hit = true: water = false
-				system("cls");
-				player1Turn = player2.shoot(x, y) ? true : false;
-				player1.printShipsBoard();
-				player2.printShootingBoard();
-				// Check gameOver
-				if (turnsCounter > minTurns && player2.allSunk())
-				{
-					cout << endl;
-					cout << "Hit, sunk and fleet destroyed! " << player1.getName() << " wins!" << endl;
-					cout << player1.getName() << " has won in a total of " << turnsCounter << " moves." << endl;
+					// Display boards
 					player1.printShipsBoard();
 					player2.printShootingBoard();
-					cout << "Congratulations " << player1.getName() << " you are the champion." << endl;
-					gameOver = true;
+					// Ask for shooting coordinates
+					cout << player1.getName() << ", it's your turn to shoot. Enter coordinates (example: B3): ";
+					char row;
+					char column;
+					cin >> row >> column;
+					int x = row - 'A';
+					int y = column - '0';
+					try
+					{
+						if (cin.fail() || !isupper(row) || row < 'A' || row > 'J')
+						{
+							throw invalid_argument("Invalid row coordinate. Please enter a valid letter between A and J in uppercase.");
+						} else if (!isdigit(column) || y < 0 || y > 9)
+						{
+							throw invalid_argument("Invalid column coordinate. Please enter a valid number between 0 and 9.");
+						} else
+						{
+							validInput = true;
+						}
+					} catch (const invalid_argument& e)
+					{
+						system("cls");
+						cerr << "\033[1;31m" << e.what() << "\033[0m" << endl;
+						cin.clear();
+						cin.ignore(numeric_limits<::streamsize>::max(), '\n');
+						continue;
+					}
+					// If player1 shoot player2. Hit = true: water = false
+					player1Turn = player2.shoot(x, y) ? true : false;
 				}
 			} else
 			{
-				// Display boards
-				player2.printShipsBoard();
-				player1.printShootingBoard();
-				// Ask for shooting coordinates
-				cout << player2.getName() << ", it's your turn to shoot. Enter coordinates (example: B3): ";
-				char row;
-				int column;
-				cin >> row >> column;
-				int x = row - 'A';
-				int y = column - '0';
-				try
+				bool validInput = false;
+				while (!validInput)
 				{
-					if (!isalpha(row) || !isupper(row) || row < 'A' || row > 'J')
-						throw invalid_argument("Invalid row coordinate. Please enter a valid letter between A and J in uppercase.");
-					if (!isdigit(column) || y < 0 || y > 9)
-						throw invalid_argument("Invalid column coordinate. Please enter a valid number between 0 and 9.");
-				} catch (const invalid_argument& e)
-				{
-					system("cls");
-					cerr << "\033[1;31m" << e.what() << "\033[0m" << endl;
-					continue;
-				}
-				// If player1 shoot player2. Hit = true: water = false
-				system("cls");
-				player1Turn = player1.shoot(x, y) ? false : true;
-				player2.printShipsBoard();
-				player1.printShootingBoard();
-				// Check gameOver
-				if (turnsCounter > minTurns && player1.allSunk())
-				{
-					cout << endl;
-					cout << "Hit, sunk and fleet destroyed! " << player2.getName() << " wins!" << endl;
-					cout << player2.getName() << " has won in a total of " << turnsCounter << " moves." << endl;
+					// Player 2 turn
+					// Display boards
 					player2.printShipsBoard();
 					player1.printShootingBoard();
-					cout << "Congratulations " << player2.getName() << " you are the champion." << endl;
-					gameOver = true;
+					// Ask for shooting coordinates
+					cout << player2.getName() << ", it's your turn to shoot. Enter coordinates (example: B3): ";
+					char row;
+					char column;
+					cin >> row >> column;
+					int x = row - 'A';
+					int y = column - '0';
+					try
+					{
+						if (cin.fail() || !isupper(row) || row < 'A' || row > 'J')
+						{
+							throw invalid_argument("Invalid row coordinate. Please enter a valid letter between A and J in uppercase.");
+						} else if (!isdigit(column) || y < 0 || y > 9)
+						{
+							throw invalid_argument("Invalid column coordinate. Please enter a valid number between 0 and 9.");
+						} else
+						{
+							validInput = true;
+						}
+					} catch (const invalid_argument& e)
+					{
+						system("cls");
+						cerr << "\033[1;31m" << e.what() << "\033[0m" << endl;
+						cin.clear();
+						cin.ignore(numeric_limits<::streamsize>::max(), '\n');
+						continue;
+					}
+					// If player1 shoot player2. Hit = true: water = false
+					player1Turn = player1.shoot(x, y) ? false : true;
 				}
+
 			}
 		} else if (menuOp == 2)
 		{
@@ -153,7 +176,28 @@ void battle(Game game)
 		{
 			cout << "Invalid choice. Please try again." << endl;
 		}
-		turnsCounter++;
+		// Check gameOver
+		if (turnsCounter > minTurns && player2.allSunk())
+		{
+			cout << endl;
+			cout << "Hit, sunk and fleet destroyed! " << player1.getName() << " wins!" << endl;
+			cout << player1.getName() << " has won in a total of " << turnsCounter << " moves." << endl;
+			player1.printShipsBoard();
+			player2.printShootingBoard();
+			cout << "Congratulations " << player1.getName() << " you are the champion." << endl;
+			gameOver = true;
+		} else if (turnsCounter > minTurns && player1.allSunk())
+		{
+			cout << endl;
+			cout << "Hit, sunk and fleet destroyed! " << player2.getName() << " wins!" << endl;
+			cout << player2.getName() << " has won in a total of " << turnsCounter << " moves." << endl;
+			player2.printShipsBoard();
+			player1.printShootingBoard();
+			cout << "Congratulations " << player2.getName() << " you are the champion." << endl;
+			gameOver = true;
+		}
+
+		++turnsCounter;
 	}
 	cout << endl;
 	cout << "GAME ENDS." << endl;
@@ -258,6 +302,7 @@ int main()
 			cerr << "\033[1;31m" << e.what() << "\033[0m" << endl;
 			cin.clear();
 			cin.ignore(numeric_limits<::streamsize>::max(), '\n');
+			continue;
 		}
 	}
 
